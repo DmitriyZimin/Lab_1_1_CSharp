edaec2cca6846a48da47cf62124a75274f2a1fbf
Dmitriy <demaxru@gmail.com>
2020-04-18 15:36:26 +0300

    Lab_1_1
    1.1. Сумма всех чётных чисел массива

---
+++ Lab_1_1/Lab_1_1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Lab_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int [] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            int result = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    result += nums[i];
+                }
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
+
