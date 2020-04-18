161331357447e63dac81b12ed00ffdcdfcc88df1
Dmitriy <demaxru@gmail.com>
2020-04-18 19:43:12 +0300

    Lab_1_2
    вывод в строке только четных символов

---
+++ Lab_1_2/Lab_1_2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab_1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            int c;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    c = nums[i];
+                    Console.WriteLine(c);
+                }
+            }
+
+        }
+    }
+}
